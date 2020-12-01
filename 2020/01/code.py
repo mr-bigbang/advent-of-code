285b06b887ad83a57baf46a20d0661db4b9d07ec
Michael Baumann <M-Baumann@outlook.com>
2020-12-01 11:46:00 +0100

    Day 01 cleanup

--- 2020/01/code.py
+++ 2020/01/code.py
@@ -12,21 +12,21 @@ def part01(expense_report: List[int]) -> int:
 
 def part02(expense_report: List[int]) -> int:
     for i in expense_report:
-        candidate = 2020 - i
         for j in expense_report:
-            partner = candidate - j
+            partner = 2020 - i - j
             if partner in expense_report:
                 return i * j * partner
 
 if __name__ == '__main__':
     with open("input.txt", "r") as f:
         expense_report = list(map(int, f.readlines()))
-        start_time = time.perf_counter()
-        print("Solution to Part 01 is:", part01(expense_report))
-        part_one = time.perf_counter()
-        print(f"Code took {part_one - start_time:0.8f}s to run")
 
-        start_time = time.perf_counter()
-        print("Solution to Part 02 is:", part02(expense_report))
-        part_two = time.perf_counter()
-        print(f"Code took {part_two - start_time:0.8f}s to run")
+    start_time = time.perf_counter()
+    print("Solution to Part 01 is:", part01(expense_report))
+    part_one = time.perf_counter()
+    print(f"Code took {part_one - start_time:0.8f}s to run")
+
+    start_time = time.perf_counter()
+    print("Solution to Part 02 is:", part02(expense_report))
+    part_two = time.perf_counter()
+    print(f"Code took {part_two - start_time:0.8f}s to run")
