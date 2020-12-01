42a282c5f05e157304dcc791bb5c08055bdec14a
Michael Baumann <M-Baumann@outlook.com>
2020-12-01 09:45:38 +0100

    Complete Day 01 Part 01

--- 2020/01/code.py
+++ 2020/01/code.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import time
+from typing import List
+
+def part01(expense_report: List[int]) -> int:
+    for e in expense_report:
+        partner = 2020 - e
+        if partner in expense_report:
+            return e * partner
+
 if __name__ == '__main__':
-    pass
+    with open("input.txt", "r") as f:
+        expense_report = list(map(int, f.readlines()))
+        start_time = time.perf_counter()
+        print("Solution to Part 01 is:", part01(expense_report))
+        part_one = time.perf_counter()
+        print(f"Code took {part_one - start_time:0.8f}s to run")
--- 2020/01/test.py
+++ 2020/01/test.py
@@ -2,9 +2,15 @@
 # -*- encoding: utf-8 -*-
 
 import unittest
+import code
 
 class TestDay01(unittest.TestCase):
-    pass
+    def test_example01(self):
+        expense_report = [1721, 299]
+        expected = 514579
+
+        result = code.part01(expense_report)
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
