cd1c0930672fbeb745bc48baddd3e2ef3b6d74f8
Michael Baumann <M-Baumann@outlook.com>
2020-12-03 14:23:35 +0100

    Make code a bit more readable

--- 2020/03/code.py
+++ 2020/03/code.py
@@ -3,22 +3,22 @@
 
 import time
 from typing import List, Tuple
+from functools import reduce
 
 def get_trees(geology: List[str], slope: Tuple[int, int]) -> int:
-    position = [0, 0]
+    x, y = 0, 0
     trees = 0
 
-    while position[1] < len(geology):
-        row = geology[position[1]]
-        if row[position[0]] == '#':
-            trees += 1
+    while y < len(geology):
+        row = geology[y]
+        trees += 1 if row[x] == '#' else 0
 
         # Add slope to position
-        position = [position[0] + slope[0], position[1] + slope[1]]
+        x += slope[0]
+        y += slope[1]
 
         # Wrap X-Axis
-        if position[0] >= len(row):
-            position[0] -= len(row)
+        x -= len(row) if x >= len(row) else 0
 
     return trees
 
@@ -26,11 +26,8 @@ def part01(geology: List[str]) -> int:
     return get_trees(geology, (3, 1))
 
 def part02(geology: List[str]) -> int:
-    return get_trees(geology, (1, 1)) * \
-           get_trees(geology, (3, 1)) * \
-           get_trees(geology, (5, 1)) * \
-           get_trees(geology, (7, 1)) * \
-           get_trees(geology, (1, 2))
+    slopes = ((1,1), (3,1), (5,1), (7,1), (1,2))
+    return reduce(lambda x, y: x * y, [get_trees(geology, s) for s in slopes])
 
 if __name__ == '__main__':
     with open("input.txt", "r") as f:
