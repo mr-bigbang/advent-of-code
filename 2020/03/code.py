703e22ea740dfaa7947eab31d9a23ff698a0d4a2
Michael Baumann <M-Baumann@outlook.com>
2020-12-03 13:04:01 +0100

    Solve AoC 2020 Day 03 Part Two

--- 2020/03/code.py
+++ 2020/03/code.py
@@ -2,10 +2,9 @@
 # -*- encoding: utf-8 -*-
 
 import time
-from typing import List
+from typing import List, Tuple
 
-def part01(geology: List[str]) -> int:
-    slope = (3, 1)
+def get_trees(geology: List[str], slope: Tuple[int, int]) -> int:
     position = [0, 0]
     trees = 0
 
@@ -23,8 +22,15 @@ def part01(geology: List[str]) -> int:
 
     return trees
 
-def part02():
-    pass
+def part01(geology: List[str]) -> int:
+    return get_trees(geology, (3, 1))
+
+def part02(geology: List[str]) -> int:
+    return get_trees(geology, (1, 1)) * \
+           get_trees(geology, (3, 1)) * \
+           get_trees(geology, (5, 1)) * \
+           get_trees(geology, (7, 1)) * \
+           get_trees(geology, (1, 2))
 
 if __name__ == '__main__':
     with open("input.txt", "r") as f:
@@ -36,6 +42,6 @@ if __name__ == '__main__':
     print(f"Code took {part_one - start_time:0.8f}s to run")
 
     start_time = time.perf_counter()
-    print("Solution to Part 02 is:", part02())
+    print("Solution to Part 02 is:", part02(geology))
     part_two = time.perf_counter()
     print(f"Code took {part_two - start_time:0.8f}s to run")
--- 2020/03/test.py
+++ 2020/03/test.py
@@ -5,26 +5,67 @@ import unittest
 import code
 
 class TestDay03(unittest.TestCase):
+    def setUp(self):
+        self.geology = ["..##.......",
+                        "#...#...#..",
+                        ".#....#..#.",
+                        "..#.#...#.#",
+                        ".#...##..#.",
+                        "..#.##.....",
+                        ".#.#.#....#",
+                        ".#........#",
+                        "#.##...#...",
+                        "#...##....#",
+                        ".#..#...#.#"]
+
     # Part 01
     def test_example01(self):
-        geology = ["..##.......",
-                   "#...#...#..",
-                   ".#....#..#.",
-                   "..#.#...#.#",
-                   ".#...##..#.",
-                   "..#.##.....",
-                   ".#.#.#....#",
-                   ".#........#",
-                   "#.##...#...",
-                   "#...##....#",
-                   ".#..#...#.#"]
         expected = 7
 
-        result = code.part01(geology)
+        result = code.part01(self.geology)
         self.assertEqual(result, expected)
 
     # Part 02
-    pass
+    def test_example02(self):
+        slope = (1, 1)
+        expected = 2
+
+        result = code.get_trees(self.geology, slope)
+        self.assertEqual(result, expected)
+
+    def test_example03(self):
+        slope = (3, 1)
+        expected = 7
+
+        result = code.get_trees(self.geology, slope)
+        self.assertEqual(result, expected)
+
+    def test_example04(self):
+        slope = (5, 1)
+        expected = 3
+
+        result = code.get_trees(self.geology, slope)
+        self.assertEqual(result, expected)
+
+    def test_example05(self):
+        slope = (7, 1)
+        expected = 4
+
+        result = code.get_trees(self.geology, slope)
+        self.assertEqual(result, expected)
+
+    def test_example06(self):
+        slope = (1, 2)
+        expected = 2
+
+        result = code.get_trees(self.geology, slope)
+        self.assertEqual(result, expected)
+
+    def test_example07(self):
+        expected = 336
+
+        result = code.part02(self.geology)
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
