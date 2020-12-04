5fa0de1fb838baed36421cc59be0e607519437ae
Michael Baumann <M-Baumann@outlook.com>
2020-12-04 14:47:00 +0100

    Tidying up

--- 2020/04/code.py
+++ 2020/04/code.py
@@ -5,12 +5,8 @@ import time
 from typing import List
 
 class Validator:
-    # ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm
     def __init__(self, passport: str):
-        self.attributes = dict()
-        for p in passport.split(" "):
-            attr = p.split(':')
-            self.attributes[attr[0]] = attr[1]
+        self.attributes = dict([p.split(':') for p in passport.strip().split(" ")])
 
     def is_valid(self) -> bool:
         required_attributes = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
@@ -18,27 +14,14 @@ class Validator:
         return False not in tests
 
 def part01(passports: List[str]) -> int:
-    validators = list()
-    passport = str()
-    for i in range(len(passports)):
-        line = passports[i]
-        if i == len(passports) - 1:
-            passport += line
-            validators.append(Validator(passport))
-        elif line == "":
-            validators.append(Validator(passport[:-1]))
-            passport = str()
-        else:
-            passport += line + " "
-
-    return len(list(filter(lambda v: v.is_valid(), validators)))
+    return len(list(filter(lambda v: v.is_valid(), map(lambda p: Validator(p), passports))))
 
 def part02():
     pass
 
 if __name__ == '__main__':
     with open("input.txt", "r") as f:
-        passports = list(map(str.strip, f.readlines()))
+        passports = [l.replace('\n', ' ') for l in f.read().split('\n\n')]
 
     start_time = time.perf_counter()
     print("Solution to Part 01 is:", part01(passports))
