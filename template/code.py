f1d4af0b7f54ae8d23ef24b747647cc2d2e7eef6
Michael Baumann <M-Baumann@outlook.com>
2020-12-04 22:50:58 +0100

    Enable selection of code to run

--- template/code.py
+++ template/code.py
@@ -11,20 +11,25 @@ def part02():
     pass
 
 if __name__ == '__main__':
+    BENCH_LOOPS = 1
+    BENCH_REPEAT = 10000
+
     with open("input.txt", "r") as f:
         pass
 
-    print("Solution to Part One is:", part01())
-    print("Solution to Part Two is:", part02())
-    
-    if "-b" in sys.argv:
-        BENCH_LOOPS = 1
-        BENCH_REPEAT = 10000
+    if "-1" in sys.argv:
+        print("Solution to Part One is:", part01())
 
-        t1 = timeit.Timer("part01()", globals=locals())
-        time_p1 = min(t1.repeat(repeat=BENCH_REPEAT, number=BENCH_LOOPS))
-        print(f"Part One: {BENCH_LOOPS} loops, best of {BENCH_REPEAT} repeats: {time_p1:0.8f}s")
+        if "-b" in sys.argv:
+            t1 = timeit.Timer("part01()", globals=locals())
+            time_p1 = min(t1.repeat(repeat=BENCH_REPEAT, number=BENCH_LOOPS))
+            print(f"Part One: {BENCH_LOOPS} loops, best of {BENCH_REPEAT} repeats: {time_p1:0.8f}s")
+    elif "-2" in sys.argv:
+        print("Solution to Part Two is:", part02())
 
-        t2 = timeit.Timer("part02()", globals=locals())
-        time_p2 = min(t2.repeat(repeat=BENCH_REPEAT, number=BENCH_LOOPS))
-        print(f"Part Two: {BENCH_LOOPS} loops, best of {BENCH_REPEAT} repeats: {time_p2:0.8f}s")
+        if "-b" in sys.argv:
+            t2 = timeit.Timer("part02()", globals=locals())
+            time_p2 = min(t2.repeat(repeat=BENCH_REPEAT, number=BENCH_LOOPS))
+            print(f"Part Two: {BENCH_LOOPS} loops, best of {BENCH_REPEAT} repeats: {time_p2:0.8f}s")
+    else:
+        print("Usage: ./code.py (-1|-2) [-b]")
