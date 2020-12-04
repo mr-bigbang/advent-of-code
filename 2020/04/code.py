aaf86a65c4aa108daf110868ae6d5933b38107aa
Michael Baumann <M-Baumann@outlook.com>
2020-12-04 16:06:45 +0100

    Solve AoC 2020 Day 04 Part Two

--- 2020/04/code.py
+++ 2020/04/code.py
@@ -13,11 +13,25 @@ class Validator:
         tests = list(map(lambda a: a in self.attributes, required_attributes))
         return False not in tests
 
+    def is_still_valid(self) -> bool:
+        if self.is_valid():
+            byr = 1920 <= int(self.attributes['byr']) <= 2002
+            iyr = 2010 <= int(self.attributes['iyr']) <= 2020
+            eyr = 2020 <= int(self.attributes['eyr']) <= 2030
+            hgt = 150 <= int(self.attributes['hgt'][:-2]) <= 193 if "cm" in self.attributes['hgt'] else 59 <= int(self.attributes['hgt'][:-2]) <= 76
+            hcl = self.attributes['hcl'][0] == '#' and int(self.attributes['eyr'][1:], 16)
+            ecl = self.attributes['ecl'] in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
+            pid = len(self.attributes['pid']) == 9
+
+            return False not in (byr, iyr, eyr, hgt, hcl, ecl, pid)
+        else:
+            return False
+
 def part01(passports: List[str]) -> int:
     return len(list(filter(lambda v: v.is_valid(), map(lambda p: Validator(p), passports))))
 
-def part02():
-    pass
+def part02(passports: List[str]) -> int:
+    return len(list(filter(lambda v: v.is_still_valid(), map(lambda p: Validator(p), passports))))
 
 if __name__ == '__main__':
     with open("input.txt", "r") as f:
@@ -29,6 +43,6 @@ if __name__ == '__main__':
     print(f"Code took {part_one - start_time:0.8f}s to run")
 
     start_time = time.perf_counter()
-    print("Solution to Part 02 is:", part02())
+    print("Solution to Part 02 is:", part02(passports))
     part_two = time.perf_counter()
     print(f"Code took {part_two - start_time:0.8f}s to run")
--- 2020/04/test.py
+++ 2020/04/test.py
@@ -11,15 +11,65 @@ class TestDay04(unittest.TestCase):
                           "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
                           "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"]
 
+        self.part02_invalid = [
+            "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+            "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946",
+            "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+            "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007"]
+
+        self.part02_valid = [
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f",
+            "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+            "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022",
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"]
+
     # Part 01
     def test_example01(self):
         expected = 2
 
         result = code.part01(self.passports)
         self.assertEqual(result, expected)
+    
+    def test_example01_0(self):
+        result = code.Validator(self.passports[0]).is_valid()
+        self.assertTrue(result)
+
+    def test_example01_1(self):
+        result = code.Validator(self.passports[1]).is_valid()
+        self.assertFalse(result)
+
+    def test_example01_2(self):
+        result = code.Validator(self.passports[2]).is_valid()
+        self.assertTrue(result)
+
+    def test_example01_3(self):
+        result = code.Validator(self.passports[3]).is_valid()
+        self.assertFalse(result)
 
     # Part 02
-    pass
+    def test_example02(self):
+        expected = 0
+
+        result = code.part02(self.part02_invalid)
+        self.assertEqual(result, expected)
+
+    def test_example02_single(self):
+        for p in self.part02_invalid:
+            with self.subTest(p):
+                result = code.Validator(p).is_still_valid()
+                self.assertFalse(result)
+
+    def test_example03(self):
+        expected = 4
+
+        result = code.part02(self.part02_valid)
+        self.assertEqual(result, expected)
+
+    def test_example03_single(self):
+        for p in self.part02_valid:
+            with self.subTest(p):
+                result = code.Validator(p).is_still_valid()
+                self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main()
