f01376f2c3b8da9c9f31b93c6dfd61cd40d9121a
Michael Baumann <M-Baumann@outlook.com>
2021-12-02 20:10:40 +0100

    Test cleanup

--- 2021/02/test.py
+++ 2021/02/test.py
@@ -5,32 +5,27 @@ import unittest
 import code
 
 class TestDay02(unittest.TestCase):
-    # Part 01
-    def test_example01(self):
-        commands = [
+    def setUp(self) -> None:
+        self.commands = [
                 "forward 5",
                 "down 5",
                 "forward 8",
                 "up 3",
                 "down 8",
                 "forward 2"]
+
+    # Part 01
+    def test_example01(self):
         expected = 150
 
-        result = code.part01(commands)
+        result = code.part01(self.commands)
         self.assertEqual(result, expected)
 
     # Part 02
     def test_example02(self):
-        commands = [
-                "forward 5",
-                "down 5",
-                "forward 8",
-                "up 3",
-                "down 8",
-                "forward 2"]
         expected = 900
 
-        result = code.part02(commands)
+        result = code.part02(self.commands)
         self.assertEqual(result, expected)
 
 if __name__ == '__main__':
