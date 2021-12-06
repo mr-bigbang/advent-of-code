b32442e590a5399c80056007e8b75807592af8c2
Michael Baumann <M-Baumann@outlook.com>
2021-12-06 14:40:28 +0100

    Solve AoC 2021 Day 06 Part Two

--- 2021/06/code.py
+++ 2021/06/code.py
@@ -6,22 +6,30 @@ import timeit
 from typing import List, Optional, Tuple
 
 
+def simulate(values: Tuple[int], days: int) -> int:
+    # Number of fish by age
+    fish = [values.count(i) for i in range(9)]
+    for _ in range(days):
+        new_fish = [0] * 9
+
+        # Age all fish
+        for i in range(1, 9):
+            new_fish[i-1] = fish[i]
+
+        # Produce new fish
+        new_fish[8] = fish[0]
+        new_fish[6] += fish[0]
+
+        fish = new_fish
+    return sum(fish)
+
+
 def part01(values: Tuple[int]) -> int:
-    lanternfish = list(values)
-    for i in range(80):
-        new_fish = []
-        for f in range(len(lanternfish)):
-            if lanternfish[f] == 0:
-                new_fish.append(8)
-                lanternfish[f] = 7
-            lanternfish[f] -= 1
-
-        lanternfish += new_fish
-    return len(lanternfish)
+    return simulate(values, 80)
 
 
-def part02(values: Tuple[str]) -> int:
-    pass
+def part02(values: Tuple[int]) -> int:
+    return simulate(values, 256)
 
 
 if __name__ == '__main__':
