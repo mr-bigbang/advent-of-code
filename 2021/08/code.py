5fa0f78c70b10c66e516b21c08335e63e71d9e95
Michael Baumann <M-Baumann@outlook.com>
2021-12-09 17:32:50 +0100

    Solve AoC 2021 Day 08 Part Two

--- 2021/08/code.py
+++ 2021/08/code.py
@@ -3,7 +3,51 @@
 
 import sys
 import timeit
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Dict
+
+
+def deduce_map(row: Tuple[str]) -> Dict[int, set]:
+    # Logic shamelessly stolen from https://github.com/BastianGanze/AdventOfCode2021/blob/master/08/src/main.rs
+    mapping = dict()
+
+    for i in row[:10]:
+        if len(i) == 2:
+            mapping['1'] = i
+        elif len(i) == 3:
+            mapping['7'] = i
+        elif len(i) == 4:
+            mapping['4'] = i
+        elif len(i) == 7:
+            mapping['8'] = i
+
+    for i in row[:10]:
+        if len(i) == 6:
+            # if 6 segments are set and only one of the segments of 1 is set, then this is the number 6
+            if len(set(i).difference(mapping['1'])) == 5:
+                mapping['6'] = i
+            # if 6 segments are set and all the segments of 4 are set, this is 9
+            elif len(set(i).difference(mapping['4'])) == 2:
+                mapping['9'] = i
+
+    for i in row[:10]:
+        # if 6 segments are set and its neither 6 nor 9 this must be 0
+        if len(i) == 6 and i not in (mapping['6'], mapping['9']):
+            mapping['0'] = i
+        elif len(i) == 5:
+            # if 5 segments are set and all 3 segments of 7 are set this is 3
+            if len(set(i).difference(mapping['7'])) == 2:
+                mapping['3'] = i
+            # if 5 segments are set and there is exactly 1 segment difference to 6 then this is 5
+            elif len(set(mapping['6']).difference(i)) == 1:
+                mapping['5'] = i
+
+    for i in row[:10]:
+        if len(i) == 5:
+            # Two is the only one left
+            if i not in (mapping['3'], mapping['5']):
+                mapping['2'] = i
+
+    return {int(x): set(y) for x, y in mapping.items()}
 
 
 def parse_input(values: Tuple[str]) -> List[List[str]]:
@@ -15,7 +59,14 @@ def part01(values: Tuple[str]) -> int:
 
 
 def part02(values: Tuple[str]) -> int:
-    pass
+    count = 0
+    for row in parse_input(values):
+        mapping = deduce_map(row)
+        for i in range(4):
+            decoded = list(mapping.keys())[list(mapping.values()).index(set(row[10+i]))]
+            count += decoded * 10**(3 - i)
+
+    return count
 
 
 if __name__ == '__main__':
--- 2021/08/test.py
+++ 2021/08/test.py
@@ -7,7 +7,7 @@ import code
 
 class TestDay08(unittest.TestCase):
     def setUp(self):
-        self.data = [
+        self.data = (
             "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
             "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
             "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
@@ -18,7 +18,11 @@ class TestDay08(unittest.TestCase):
             "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
             "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
             "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
-        ]
+        )
+
+        self.data1 = (
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
+        )
 
         with open("input.txt", "r") as f:
             self.input = tuple(f.readlines())
@@ -37,17 +41,23 @@ class TestDay08(unittest.TestCase):
         self.assertEqual(expected, result)
 
     # Part 02
-    # def test_example02(self):
-    #     expected = None
-    #
-    #     result = code.part02(self.data)
-    #     self.assertEqual(expected, result)
-    #
-    # def test_result02(self):
-    #     expected = None
-    #
-    #     result = code.part02(self.input)
-    #     self.assertEqual(expected, result)
+    def test_example02(self):
+        expected = 5353
+
+        result = code.part02(self.data1)
+        self.assertEqual(expected, result)
+
+    def test_example03(self):
+        expected = 61229
+
+        result = code.part02(self.data)
+        self.assertEqual(expected, result)
+
+    def test_result02(self):
+        expected = 1073431
+
+        result = code.part02(self.input)
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
